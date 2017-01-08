a08b232c0b88b2cdda4f353ffaa9aa61d0084e07
Prasanna Subramanian <prasanna@webplumbr.com>
2017-01-08 13:41:07 +1300

    adding documentation to code

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -13,6 +13,12 @@ use App\Http\Controllers\Controller;
 
 class ApiController extends Controller
 {
+    /**
+     * Fetch the details of a library provided by its identifier
+     *
+     * @param integer $id The unique identifier of a library
+     * @return JSON object|404 not found response
+     */
     public function libraryDetails($id)
     {
         try {
@@ -22,17 +28,29 @@ class ApiController extends Controller
         }
     }
 
+    /**
+     * Creates a new library
+     *
+     * @param LibrarySaveRequest $request
+     * @return JSON object
+     */
     public function librarySave(LibrarySaveRequest $request)
     {
         //only allows saving a new record
         return Library::create($request->all());
     }
 
+    /**
+     * Parses a binary tree and returns the smallest value of the leaf
+     *
+     * @param Request $request
+     * @return integer
+     */
     public function findSmallestLeaf(Request $request)
     {
         $tree = json_decode($request->input('tree', null));
 
-        if (json_last_error() === JSON_ERROR_NONE) {
+        if (json_last_error() === JSON_ERROR_NONE && is_object($tree)) {
             return get_smallest_leaf($tree);
         } else {
             abort(400);
--- app/Lib/Helpers.php
+++ app/Lib/Helpers.php
@@ -1,12 +1,25 @@
 <?php
+//Helper functions
 
-
+/**
+ * Find the smallest value of the leaf in a binary tree
+ *
+ * @param stdClass $tree
+ * @return integer
+ */
 function get_smallest_leaf($tree)
 {
     $leaves = has_children($tree);
     return is_array($leaves)? min(get_values($leaves)) : $leaves;
 }
 
+/**
+ * Accepts a multi-dimensional array and converts it to a flat array with values
+ * key=value relationship is NOT maintained
+ *
+ * @param multi-dimensional array $arr
+ * @return array
+ */
 function get_values($arr)
 {
     $values = [];
@@ -20,6 +33,13 @@ function get_values($arr)
     return $values;
 }
 
+/**
+ * Accepts a binary tree as input and returns the identified leaf|leaves
+ *
+ * @param stdClass $node
+ * @return array|integer
+ *
+ */
 function has_children($node)
 {
     if (isset($node->left) && isset($node->right)) {
